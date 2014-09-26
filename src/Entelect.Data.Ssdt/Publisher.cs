1f2140f898d7d29bde4f9a407a9f95436b0a5c2f
Ryan Kotzen <Ryank@entelect.co.za>
2014-09-26 15:44:43 +0200

    adding curley braces

--- src/Entelect.Data.Ssdt/Publisher.cs
+++ src/Entelect.Data.Ssdt/Publisher.cs
@@ -8,7 +8,9 @@ namespace Entelect.Data.Ssdt
         {
             var x86Path = Environment.GetEnvironmentVariable("programfiles(x86)");
             if (!string.IsNullOrWhiteSpace(x86Path))
+            {
                 return x86Path;
+            }
             var x64Path = Environment.GetEnvironmentVariable("programfiles");
             return x64Path;
         }
