5e033e721d8c05d645c7841d264eef078f64f69c
Ryan Kotzen <Ryank@entelect.co.za>
2014-09-26 15:59:23 +0200

    adding more tests

--- src/Entelect.Data.Ssdt/Publisher.cs
+++ src/Entelect.Data.Ssdt/Publisher.cs
@@ -14,5 +14,28 @@ namespace Entelect.Data.Ssdt
             var x64Path = Environment.GetEnvironmentVariable("programfiles");
             return x64Path;
         }
+
+        /// <summary>
+        /// Publishes the db using the supplied path to the .dacpac file and path to the .xml publish file
+        /// </summary>
+        /// <param name="dacpacPath"></param>
+        /// <param name="publishFilePath"></param>
+        public void Publish(string dacpacPath, string publishFilePath)
+        {
+            ValidateInputs(dacpacPath, publishFilePath);
+            throw new NotImplementedException();
+        }
+
+        private static void ValidateInputs(string dacpacPath, string publishFilePath)
+        {
+            if (string.IsNullOrWhiteSpace(dacpacPath))
+            {
+                throw new ArgumentNullException("dacpacPath", "Dacpac Path cannot be null");
+            }
+            if (string.IsNullOrWhiteSpace(publishFilePath))
+            {
+                throw new ArgumentNullException("publishFilePath", "Publish File Path cannot be null");
+            }
+        }
     }
 }
--- test/Entelect.Data.Ssdt.Tests/Publisher.Tests.cs
+++ test/Entelect.Data.Ssdt.Tests/Publisher.Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Entelect.Data.Ssdt.Tests
 {
@@ -11,6 +12,44 @@ namespace Entelect.Data.Ssdt.Tests
             var publisher = new Publisher();
             var programFilesPath = publisher.GetProgramfilesPath();
             Assert.False(string.IsNullOrWhiteSpace(programFilesPath));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DacpacPathCannotBeNull()
+        {
+            var publisher = new Publisher();
+            const string dacpacPath = null;
+            const string publishFilePath = "asd";
+            publisher.Publish(dacpacPath, publishFilePath);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PublishFilePathCannotBeNull()
+        {
+            var publisher = new Publisher();
+            const string dacpacPath = "asd";
+            const string publishFilePath = null;
+            publisher.Publish(dacpacPath, publishFilePath);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DacpacPathCannotBeWhitespace()
+        {
+            var publisher = new Publisher();
+            const string publishFilePath = "asd";
+            publisher.Publish(string.Empty, publishFilePath);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PublishFilePathCannotBeWhitespace()
+        {
+            var publisher = new Publisher();
+            const string dacpacPath = "asd";
+            publisher.Publish(dacpacPath, string.Empty);
         }
     }
 }
