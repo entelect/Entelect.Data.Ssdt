f88aba9b9de5bff537544b1b6767e3bf9ceedbbb
Ryan Kotzen <Ryank@entelect.co.za>
2014-09-26 16:19:16 +0200

    Adding code to ensure that sqlpackage.exe exists.

--- src/Entelect.Data.Ssdt/Publisher.cs
+++ src/Entelect.Data.Ssdt/Publisher.cs
@@ -1,20 +1,10 @@
 ﻿using System;
+using System.IO;
 
 namespace Entelect.Data.Ssdt
 {
     public class Publisher
     {
-        internal protected virtual string GetProgramfilesPath()
-        {
-            var x86Path = Environment.GetEnvironmentVariable("programfiles(x86)");
-            if (!string.IsNullOrWhiteSpace(x86Path))
-            {
-                return x86Path;
-            }
-            var x64Path = Environment.GetEnvironmentVariable("programfiles");
-            return x64Path;
-        }
-
         /// <summary>
         /// Publishes the db using the supplied path to the .dacpac file and path to the .xml publish file
         /// </summary>
@@ -23,7 +13,33 @@ namespace Entelect.Data.Ssdt
         public void Publish(string dacpacPath, string publishFilePath)
         {
             ValidateInputs(dacpacPath, publishFilePath);
+            var pathToSqlpackageExe = GetPathToSqlpackageExe();
+            if (!File.Exists(pathToSqlpackageExe))
+            {
+                throw new FileNotFoundException(string.Format("Sqlpackage.exe not found at path {0}, SSDT must be installed", pathToSqlpackageExe), "Sqlpackage.exe");
+            }
             throw new NotImplementedException();
+        }
+
+        internal protected virtual string GetProgramfilesPath()
+        {
+            var x86Path = Environment.GetEnvironmentVariable("programfiles(x86)");
+            if (!string.IsNullOrWhiteSpace(x86Path))
+            {
+                return x86Path;
+            }
+            var x64Path = Environment.GetEnvironmentVariable("programfiles");
+            if (!string.IsNullOrWhiteSpace(x64Path))
+            {
+                return x64Path;
+            }
+            throw new ArgumentException("Unable to determin path to the program files folder, you must set either the \"programfiles(x86)\" or \"programfiles\" environment variable");
+        }
+
+        protected internal virtual string GetPathToSqlpackageExe()
+        {
+            var programFilesPath = GetProgramfilesPath();
+            return string.Format(@"{0}\Microsoft SQL Server\110\DAC\bin\sqlpackage.exe", programFilesPath);
         }
 
         private static void ValidateInputs(string dacpacPath, string publishFilePath)
--- test/Entelect.Data.Ssdt.Tests/Publisher.Tests.cs
+++ test/Entelect.Data.Ssdt.Tests/Publisher.Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace Entelect.Data.Ssdt.Tests
@@ -50,6 +51,25 @@ namespace Entelect.Data.Ssdt.Tests
             var publisher = new Publisher();
             const string dacpacPath = "asd";
             publisher.Publish(dacpacPath, string.Empty);
+        }
+
+        [Test]
+        public void CheckSqlpackageExePathCorrect()
+        {
+            var publisher = new Publisher();
+            var pathToSqlpackageExe = publisher.GetPathToSqlpackageExe();
+            Assert.False(string.IsNullOrWhiteSpace(pathToSqlpackageExe));
+            Assert.True(File.Exists(pathToSqlpackageExe), string.Format("SqlpackageExe not found at path: {0}", pathToSqlpackageExe));
+        }
+
+
+        [Test]
+        public void AbsoluteDacpacAndPublishFilePath()
+        {
+            var publisher = new Publisher();
+            const string dacpacPath = @"C:\Development\EntelectOpenSorceLibrary\Entelect.Data.Ssdt\test\TestDatabase\bin\Debug";
+            const string publishFilePath = @"C:\Development\EntelectOpenSorceLibrary\Entelect.Data.Ssdt\test\TestDatabase\TestDatabase.LocalDb.publish.xml";
+            publisher.Publish(dacpacPath, publishFilePath);
         }
     }
 }
