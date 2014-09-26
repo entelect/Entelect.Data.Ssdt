e00d9e7192441bcba3260ae26878ffbdc087ac0d
Ryan Kotzen <Ryank@entelect.co.za>
2014-09-26 15:20:48 +0200

    Adding tests and initial code

---
+++ src/Entelect.Data.Ssdt/Publisher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Entelect.Data.Ssdt
+{
+    public class Publisher
+    {
+
+        public void Publish()
+        {
+            
+        }
+
+        internal protected virtual string GetProgramfilesPath()
+        {
+            var x86Path = Environment.GetEnvironmentVariable("programfiles(x86)");
+            if (!string.IsNullOrWhiteSpace(x86Path))
+                return x86Path;
+            var x64Path = Environment.GetEnvironmentVariable("programfiles");
+            return x64Path;
+        }
+    }
+}
---
+++ test/Entelect.Data.Ssdt.Tests/Publisher.Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Entelect.Data.Ssdt.Tests
+{
+    [TestFixture]
+    public class PublisherTests
+    {
+        [Test]
+        public void GetProgramfilesPath()
+        {
+            var publisher = new Publisher();
+            var programFilesPath = publisher.GetProgramfilesPath();
+            Assert.False(string.IsNullOrWhiteSpace(programFilesPath));
+        }
+    }
+}
