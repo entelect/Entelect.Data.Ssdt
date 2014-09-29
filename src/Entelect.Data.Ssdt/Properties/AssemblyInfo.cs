c60684eafd7419d92adf17f05e654ef5f3e24f23
Ryan Kotzen <Ryank@entelect.co.za>
2014-09-29 10:36:16 +0200

    Updating License url.
    Updating assemblyinfo

--- src/Entelect.Data.Ssdt/Properties/AssemblyInfo.cs
+++ src/Entelect.Data.Ssdt/Properties/AssemblyInfo.cs
@@ -6,14 +6,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Entelect.Data.Ssdt")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Library to deploy an ssdt project to a database")]
+[assembly: AssemblyCompany("Entelect")]
 [assembly: AssemblyProduct("Entelect.Data.Ssdt")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Entelect Software 2014")]
+[assembly: AssemblyTrademark("Entelect")]
+[assembly: AssemblyCulture("EN-ZA")]
 [assembly: InternalsVisibleTo("Entelect.Data.Ssdt.Tests")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -33,4 +37,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
